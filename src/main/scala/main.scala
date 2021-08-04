dabe5932da88053f026a8ecb74c418753e4cea76
Joana <joanapiovaroli@gmail.com>
2021-08-04 20:14:47 -0300

    Add formatting

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,10 +4,12 @@ object main {
   def main(args: Array[String]): Unit = {
 //    val nombre = args(0)
 //    println("hola mi nombre es " + nombre)
+
 //      val estado = args(0).length()
 //      val r = scala.util.Random
 //      val d = r.nextInt(3)
 //      println(estado / d)
+
     def toInt(s: String): Int = {
       try {
         s.toInt
