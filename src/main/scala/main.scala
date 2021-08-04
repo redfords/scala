63af17f4cd65b3ca1707bb7309387fae2024f0ab
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-08-04 20:02:39 -0300

    Add toInt

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,28 @@
+import scala.annotation.switch
+
 object main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World")
-  }
+//    val nombre = args(0)
+//    println("hola mi nombre es " + nombre)
+//      val estado = args(0).length()
+//      val r = scala.util.Random
+//      val d = r.nextInt(3)
+//      println(estado / d)
+    def toInt(s: String): Int = {
+      try {
+        s.toInt
+      } catch {
+        case e: Exception => -1
+      }
+    }
 
+    def resultado(n: Int): String = {
+      if (n == 0) "par"
+      else if (n > 0) "impar"
+      else "no es un numero"
+    }
+
+    val n = toInt(args(0)) % 2
+    println(resultado(n))
+  }
 }
