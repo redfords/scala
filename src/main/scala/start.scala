bd2b2bb6895131b1f1a26d4123e7159cb2e219ec
Joana <joanapiovaroli@gmail.com>
2021-08-27 23:24:51 -0300

    Add fligh data example

--- src/main/scala/start.scala
+++ src/main/scala/start.scala
@@ -7,4 +7,22 @@ object start {
     .config("spark.some.config.option", "some-value")
     .getOrCreate()
 
+  val flightData2015 = spark
+    .read
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .csv("/data/flight-data/csv/2015-summary.csv")
+
+  val sqlWay = spark.sql("""
+  SELECT DEST_COUNTRY_NAME, count(1)
+  FROM flight_data_2015
+  GROUP BY DEST_COUNTRY_NAME
+  """)
+  val dataFrameWay = flightData2015
+    .groupBy(Symbol("DEST_COUNTRY_NAME"))
+    .count()
+
+  sqlWay.explain
+  dataFrameWay.explain
+
 }
