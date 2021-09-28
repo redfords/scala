e06806b19c8956d265428b9123d736da5cb9a689
Joana <joanapiovaroli@gmail.com>
2021-09-27 23:36:05 -0300

    Add case class person

--- src/main/scala/dataset.scala
+++ src/main/scala/dataset.scala
@@ -1,3 +1,29 @@
 object dataset {
+  case class Person(name: String, age: Long)
+
+  // Encoders are created for case classes
+  val caseClassDS = Seq(Person("Andy", 32)).toDS()
+  caseClassDS.show()
+  // +----+---+
+  // |name|age|
+  // +----+---+
+  // |Andy| 32|
+  // +----+---+
+
+  // Encoders for most common types are automatically provided by importing spark.implicits._
+  val primitiveDS = Seq(1, 2, 3).toDS()
+  primitiveDS.map(_ + 1).collect() // Returns: Array(2, 3, 4)
+
+  // DataFrames can be converted to a Dataset by providing a class. Mapping will be done by name
+  val path = "examples/src/main/resources/people.json"
+  val peopleDS = spark.read.json(path).as[Person]
+  peopleDS.show()
+  // +----+-------+
+  // | age|   name|
+  // +----+-------+
+  // |null|Michael|
+  // |  30|   Andy|
+  // |  19| Justin|
+  // +----+-------+
 
 }
