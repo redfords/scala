776ee6d3758ba0721979f23eaa9bcd7d78d42f13
Joana <joanapiovaroli@gmail.com>
2021-09-28 23:50:50 -0300

    Create dataset from text file

--- src/main/scala/dataset.scala
+++ src/main/scala/dataset.scala
@@ -26,4 +26,16 @@ object dataset {
   // |  19| Justin|
   // +----+-------+
 
+  // Create an RDD of Person objects from a text file, convert it to a Dataframe
+  val peopleDF = spark.sparkContext
+    .textFile("examples/src/main/resources/people.txt")
+    .map(_.split(","))
+    .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
+    .toDF()
+  // Register the DataFrame as a temporary view
+  peopleDF.createOrReplaceTempView("people")
+
+  // SQL statements can be run by using the sql methods provided by Spark
+  val teenagersDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 19")
+
 }
