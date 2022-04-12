2f83996b169a092728c88c581a812ad7f985202a
redfords <joanapiovaroli@gmail.com>
2022-04-11 23:57:14 -0300

    Add exerises

--- src/main/scala/conditionals.scala
+++ src/main/scala/conditionals.scala
@@ -1,17 +1,27 @@
 object conditionals {
 
-//  Convert the temperature value of 22.5 Centigrade to Fahrenheit.
-//  The conversion formula is cToF(x) = (x * 9/5) + 32
+//  Given a string name , write a match expression that will return the same string if
+//  nonempty, or else the string “n/a” if it is empty
 
-//  Modify the Centigrade-to-Fahrenheit formula to return an integer instead of a
-//  floating-point number
+//  Given a double amount , write an expression to return “greater” if it is more than
+//  zero, “same” if it equals zero, and “less” if it is less than zero. Can you write this with
+//  if..else blocks? How about with match expressions?
 
-//  Using the input value 2.7255 , generate the string “You owe $2.73.” Is this doable
-//  with string interpolation?
+//  Write an expression to convert one of the input values cyan , magenta , yellow to
+//  their six-char hexadecimal equivalents in string form. What can you do to handle
+//  error conditions?
 
-//  Using the input string “Frank,123 Main,925-555-1943,95122” and regular expres‐
-//  sion matching, retrieve the telephone number. Can you convert each part of the
-//  telephone number to its own integer value? How would you store this in a tuple?
+//  Print the numbers 1 to 100, with each line containing a group of five numbers. For
+//  example:
+//  1, 2, 3, 4, 5,
+//  6, 7, 8, 9, 10
+//  ....
+
+//  Write an expression to print the numbers from 1 to 100, except that for multiples
+//  of 3, print “type,” and for multiples of 5, print “safe.” For multiples of both 3 and 5,
+//  print “typesafe.”
+
+//  Can you rewrite the answer to exercise 5 to fit on one line?
 
 
 }
