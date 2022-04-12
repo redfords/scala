5d76e2193a266f7c136353c8b1b475974d087298
redfords <joanapiovaroli@gmail.com>
2022-04-11 23:55:10 -0300

    Create conditionals.scala

---
+++ src/main/scala/conditionals.scala
@@ -0,0 +1,17 @@
+object conditionals {
+
+//  Convert the temperature value of 22.5 Centigrade to Fahrenheit.
+//  The conversion formula is cToF(x) = (x * 9/5) + 32
+
+//  Modify the Centigrade-to-Fahrenheit formula to return an integer instead of a
+//  floating-point number
+
+//  Using the input value 2.7255 , generate the string “You owe $2.73.” Is this doable
+//  with string interpolation?
+
+//  Using the input string “Frank,123 Main,925-555-1943,95122” and regular expres‐
+//  sion matching, retrieve the telephone number. Can you convert each part of the
+//  telephone number to its own integer value? How would you store this in a tuple?
+
+
+}
