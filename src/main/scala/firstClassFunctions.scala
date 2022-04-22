db9930abce5703666b6ad5885378d7acbaf3f120
redfords <joanapiovaroli@gmail.com>
2022-04-21 23:41:58 -0300

    Remove exercises

--- src/main/scala/firstClassFunctions.scala
+++ src/main/scala/firstClassFunctions.scala
@@ -17,38 +17,5 @@ object firstClassFunctions {
   // invoking high-order functions with function literal blocks
 
 
-//  Write a function that computes the area of a circle given its radius.
-
-//  Provide an alternate form of the function in exercise 1 that takes the radius as a
-//  String . What happens if your function is invoked with an empty String ?
-
-//  Write a recursive function that prints the values from 5 to 50 by fives, without using
-//  for or while loops. Can you make it tail-recursive?
-
-//  Write a function that takes a milliseconds value and returns a string describing the
-//  value in days, hours, minutes, and seconds. What’s the optimal type for the input
-//  value?
-
-//  Write a function that calculates the first value raised to the exponent of the second
-//  value. Try writing this first using math.pow , then with your own calculation. Did
-//  you implement it with variables? Is there a solution available that only uses im‐
-//  mutable data? Did you choose a numeric type that is large enough for your uses?
-
-//  Write a function that calculates the difference between a pair of 2D points (x and
-//  y) and returns the result as a point. Hint: this would be a good use for tuples (see
-//  “Tuples” on page 25).
-
-//  Write a function that takes a 2-sized tuple and returns it with the Int value (if
-//  included) in the first position. Hint: this would be a good use for type parameters
-//  and the isInstanceOf type operation.
-
-//  Write a function that takes a 3-sized tuple and returns a 6-sized tuple, with each
-//  original parameter followed by its String representation. For example, invoking
-//  the function with (true, 22.25, "yes") should return (true, "true", 22.5,
-//  "22.5", "yes", "yes") . Can you ensure that tuples of all possible types are com‐
-//  patible with your function? When you invoke this function, can you do so with
-//  explicit types not only in the function result but in the value that you use to store
-//  the result?
-
 
 }
