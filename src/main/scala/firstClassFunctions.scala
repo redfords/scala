f6eef4adb2974e1fc5cedcffcbb74a6d91c0869a
redfords <joanapiovaroli@gmail.com>
2022-04-22 23:31:44 -0300

    Add function types

--- src/main/scala/firstClassFunctions.scala
+++ src/main/scala/firstClassFunctions.scala
@@ -1,6 +1,11 @@
 object firstClassFunctions {
 
   // function types and values
+  // The type of a function is a simple grouping of its input types and return value type,
+  // arranged with an arrow indicating the direction from input types to output type.
+
+  def double(x: Int): Int = x * 2
+  val myDouble: (Int) => Int = double
 
   // higher-order functions
 
