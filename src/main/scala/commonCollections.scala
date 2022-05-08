8c162da6e554aacb0d6d497fa20b9cbb1684434a
redfords <joanapiovaroli@gmail.com>
2022-05-07 23:33:18 -0300

    Add subtitles

--- src/main/scala/commonCollections.scala
+++ src/main/scala/commonCollections.scala
@@ -1,3 +1,21 @@
 object commonCollections {
 
+  // lists, sets and maps
+
+  // what's in a list
+
+  // the cons operator
+
+  // list arithmetic
+
+  // mapping lists
+
+  // reducing lists
+
+  // converting collections
+
+  // pattern matching with collections
+
+
+
 }
